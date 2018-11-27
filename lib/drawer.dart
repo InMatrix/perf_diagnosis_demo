f8e8fcf6ebedb5d88a14cc176348f0c85cbd03ef
kenzieschmoll <kenzieschmoll@google.com>
2018-11-27 13:39:28 -0800

    Initial Json parsing code sample.

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -4,6 +4,7 @@ import 'package:perf_diagnosis_demo/optimized/spinning_box_fix.dart';
 import 'many_materials_demo.dart';
 import 'opacity_demo.dart';
 import 'optimized/clock_fix.dart';
+import 'optimized/json_fix.dart';
 import 'optimized/list_fix.dart';
 import 'optimized/scorecard_fix.dart';
 
@@ -29,6 +30,7 @@ class MyDrawer extends StatelessWidget {
           _makeDemoEntry(context, "List Demo", ListFix()),
           _makeDemoEntry(context, "Spinning Box Demo", SpinningBoxFix()),
           _makeDemoEntry(context, "Scorecard Demo", ScorecardFix()),
+          _makeDemoEntry(context, "JSON Demo", JsonFix()),
           Divider(),
           Align(
             alignment: Alignment.centerLeft,
